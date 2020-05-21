323a377d373cb18276a0e7bf4cf2c616108497c9
Samuel Michalik <samuelmichalik@yahoo.com>
2020-05-21 16:30:14 +0200

    add new architectures

--- attndec_batched.py
+++ attndec_batched.py
@@ -6,19 +6,71 @@ import torch.nn.functional as F
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+class AdditiveAttention(nn.Module):
+
+    def __init__(self, dim_k, dim_q, dropout_p=0.1):
+        """Creates an additive attention layer.
+
+        Args:
+            dim_k: Dimension of the key.
+            dim_q: Dimension of the query.
+        """
+
+        super(AdditiveAttention, self).__init__()
+        self.linear_key = nn.Linear(dim_k, 1)
+        self.linear_query = nn.Linear(dim_q, 1)
+        self.dropout = nn.Dropout(p=dropout_p)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, Q, K, V):
+        """Computes the additive attention function.
+
+        Args:
+            Q: queries of shape [batch, dim_q]
+            K: keys of shape [batch, n_keys, dim_k]
+            V: values of shape [batch, n_keys, dim_v]
+        """
+        
+        n_keys = K.size()[1]
+        tk = self.linear_key(K)                 # tk: [batch, n_keys, 1]
+        tq = self.linear_query(Q)               # tq: [batch, 1]
+        tq = tq.expand(-1, n_keys)              # tq: [batch, n_keys]
+        tq = tq.unsqueeze(2)                    # tq: [batch, n_keys, 1]
+        weights = self.softmax(tk + tq)
+        weights = weights.permute(0, 2, 1)      # weights: [batch, 1, n_keys]
+        output = torch.bmm(weights, V)          # output: [batch, 1, dim_v]
+        
+        weights = weights.squeeze(1)
+        output = output.squeeze(1)
+        return output, weights
+
+
+
+class DotProductAttention(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, Q, K, V):
+
+    
+        weights = F.softmax(None, dim=1)
+
+        return
+
 class Decoder(nn.Module):
 
-    def __init__(self, hidden_size, output_size, dropout_p=0.1):
+    def __init__(self, hid_dim, out_dim, dropout_p=0.1):
         super(Decoder, self).__init__()
-        self.hidden_size = hidden_size
-        self.output_size = output_size
+        self.hid_dim = hid_dim
+        self.out_dim = out_dim
         self.dropout_p = dropout_p
 
-        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.embedding = nn.Embedding(self.out_dim, self.hid_dim)
         
-        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.gru = nn.GRU(hid_dim, hid_dim)
         
-        self.out = nn.Linear(hidden_size, output_size)
+        self.out = nn.Linear(hid_dim, out_dim)
         
         self.log_softmax = nn.LogSoftmax(dim=2)
 
@@ -30,26 +82,27 @@ class Decoder(nn.Module):
         return o, h
 
     def initHidden(self, batch_size):
-        return torch.zeros(1, batch_size, self.hidden_size, device=device)
+        return torch.zeros(1, batch_size, self.hid_dim, device=device)
 
 class AttentionDecoder(nn.Module):
 
-    def __init__(self, hidden_size, output_size, encoder_dim=(None, None), dropout_p=0.1):
+    def __init__(self, hid_dim, out_dim, n_keys, key_dim, dropout_p=0.1, **kwargs):
         super(AttentionDecoder, self).__init__()
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.encoder_dim = encoder_dim
+        self.hid_dim = hid_dim
+        self.out_dim = out_dim
+        self.n_keys = n_keys
+        self.key_dim = key_dim
         self.dropout_p = dropout_p
 
-        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.embedding = nn.Embedding(self.out_dim, self.hid_dim)
         
-        self.attn = nn.Linear(self.hidden_size * 2, self.encoder_dim[0])
-        self.attn_combine = nn.Linear(self.hidden_size + self.encoder_dim[1], self.hidden_size)
+        self.attn = nn.Linear(self.hid_dim * 2, n_keys)
+        self.attn_combine = nn.Linear(self.hid_dim + key_dim, self.hid_dim)
         self.dropout = nn.Dropout(self.dropout_p)
 
-        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.gru = nn.GRU(hid_dim, hid_dim)
         
-        self.out = nn.Linear(hidden_size, output_size)
+        self.out = nn.Linear(hid_dim, out_dim)
         
         self.log_softmax = nn.LogSoftmax(dim=2)
 
@@ -57,17 +110,13 @@ class AttentionDecoder(nn.Module):
         """
         Shapes:
             input: [1, batch_size, 1] ([seq_len, batch, index])
-            hidden: [1, batch_size, hidden_size]
+            hidden: [1, batch_size, hid_dim]
             encoder_output: [batch_size, enc_dim[0], enc_dim[1]]
         """
 
-        # print("input", input.size())
-        # print("hidden0", hidden.size())
         o = self.embedding(input).squeeze(dim=2)
         o = self.dropout(o)
-        # print("embedded", o.size())
 
-        # Attention
         attn_input = torch.cat((o[0], hidden[0]), dim=1)
         # attn_input size is [batch, emb_dim + hid_dim]
 
@@ -92,12 +141,8 @@ class AttentionDecoder(nn.Module):
 
         o = F.relu(o)
         o, h = self.gru(o, hidden)
-        # print("gru output ", o.size())
-        # print("gru hidden ", h.size())
         o = self.out(o)
-        # print("linear output projection output ", o.size())
         o = self.log_softmax(o)
-        # print("softmax output ", o.size())
         
         a = attn_weights.squeeze(dim=1)
         # a : [batch, enc_dim[0]]
@@ -106,29 +151,89 @@ class AttentionDecoder(nn.Module):
         return o, h, a
 
     def initHidden(self, batch_size):
-        return torch.zeros(1, batch_size, self.hidden_size, device=device)
+        return torch.zeros(1, batch_size, self.hid_dim, device=device)
+
+class AttentionDecoder_2(nn.Module):
+
+    def __init__(self, hid_dim, emb_dim, out_dim, key_dim, val_dim, dropout_p=0.1, **kwargs):
+        super(AttentionDecoder_2, self).__init__()
+        self.hid_dim = hid_dim
+        self.emb_dim = emb_dim
+        self.dropout_p = dropout_p
+        
+        self.embedding = nn.Embedding(out_dim, emb_dim)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.attention = AdditiveAttention(key_dim, hid_dim)
+        self.gru = nn.GRU(emb_dim + val_dim, hid_dim)
+        self.w_h = nn.Linear(hid_dim, emb_dim)
+        self.w_z = nn.Linear(val_dim, emb_dim)
+        self.out = nn.Linear(emb_dim, out_dim)
+        self.log_softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden, annotations):
+        """
+        Args:
+            input: [1, batch, 1]
+            hidden: [1, batch, hid_dim]
+            annotations: [batch, n_keys, key_dim]
+        """
+
+        emb = self.embedding(input).squeeze(dim=2)  # emb: [1, batch, emb_dim]
+        emb = self.dropout(emb)
+
+        context, attn_weights = self.attention(Q=hidden.squeeze(dim=0), 
+                                    K=annotations, 
+                                    V=annotations)
+        
+        gru_in = torch.cat((emb, context.unsqueeze(dim=0)), dim=2)  # gru_in: [1, batch, emb_dim + val_dim]
+        out, hid = self.gru(gru_in, hidden)
+
+        # deep output layer
+        z = self.w_z(context)       # z: [batch, emb_dim]
+        h = self.w_h(hid)           # h: [batch, emb_dim]
+        out = self.out(emb.squeeze(dim=0) + z + h) # out: [batch, out_dim]
+
+        out = self.log_softmax(out)
+        return out, hid, attn_weights
+
+    def initHidden(self, batch_size):
+        return torch.zeros(1, batch_size, self.hid_dim, device=device)
+
 
 class Network(nn.Module):
 
     def __init__(
         self,
-        hidden_size,
-        output_size,
+        hid_dim,
+        out_dim,
+        emb_dim,
+        enc_seq_len,
+        enc_dim,
         sos_token, 
         eos_token, 
         pad_token,
-        teacher_forcing_rat=0.2
+        teacher_forcing_rat=0.2,
+        decoder=AttentionDecoder
         ):
 
         super(Network, self).__init__()
-        self.hidden_size = hidden_size
-        self.output_size = output_size
+        self.hid_dim = hid_dim
+        self.out_dim = out_dim
+        self.emb_dim = emb_dim
+        self.enc_seq_len = enc_seq_len
+        self.enc_dim = enc_dim
         self.sos_token = sos_token
         self.eos_token = eos_token
         self.pad_token = pad_token
         self.teacher_forcing_rat = teacher_forcing_rat
-        self.decoder = AttentionDecoder(hidden_size, output_size, 
-            encoder_dim=(14 * 14, 512))
+
+        self.decoder = decoder(hid_dim=hid_dim, 
+                        emb_dim=emb_dim, 
+                        out_dim=out_dim, 
+                        key_dim=enc_dim, 
+                        val_dim=enc_dim,
+                        n_keys=enc_seq_len)
+
         self.decoder.to(device)
 
     def forward(self, features, targets=None, max_len=10):
@@ -138,20 +243,15 @@ class Network(nn.Module):
             targets: [max_len, batch_size, 1]
         """
 
-        # print(features.size())
-        # if targets is not None: print(targets.size())
-
         # features : [batch, enc_seq_len, enc_dim]
         batch_size = features.size()[0]
-        enc_dim_1 = features.size()[1]
-        enc_dim_2 = features.size()[2]
         
         y = torch.tensor([[self.sos_token]] * batch_size, device=device).view(1, batch_size, 1)
         hid = self.decoder.initHidden(batch_size=batch_size)
         
         # gradually store outputs here:
-        outputs = torch.zeros(max_len, batch_size, self.output_size, device=device)
-        attentions = torch.zeros(max_len, batch_size, enc_dim_1, device=device)
+        outputs = torch.zeros(max_len, batch_size, self.out_dim, device=device)
+        attentions = torch.zeros(max_len, batch_size, self.enc_seq_len, device=device)
 
         for i in range(max_len):
             out, hid, att = self.decoder(y, hid, features)
@@ -168,13 +268,14 @@ class Network(nn.Module):
         
         return outputs, attentions # output logits in shape [max_len, batch, vocab]
 
+
+
     def infere(self, features, max_len=10):
         out, att = self.forward(features, targets=None, max_len=max_len)
         out = out.permute(1, 0, 2).detach()
         att = att.permute(1, 0, 2).detach()
 
         _, topi = y.topk(1, dim=2)
-        # topi : [batch, max_len, 1]
         topi = topi.squeeze(2)
 
         return {
